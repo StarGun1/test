4d4f9edf4cb032274c18b794dd2f8caf39c25205
StarGun1 <58214849+StarGun1@users.noreply.github.com>
2023-09-23 02:43:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import time
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from flask import Flask
+from selenium import webdriver
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
-    with open("test.txt", 'w') as f:
-        f.write("test")
-    with open("test.txt", 'r') as f:
-        c = f.readlines()
-    return c
-  
+def scrape_example():
+    options = Options()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.get('http://www.example.com')
+    button = driver.find_element(By.TAG_NAME, 'a')
+    button.click()
+    text = driver.find_element(By.TAG_NAME, 'p').text
+    driver.quit()
+    return text
