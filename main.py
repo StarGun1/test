2ca0630320893ba0847b3b55f6958a6c2304c1d1
StarGun1 <58214849+StarGun1@users.noreply.github.com>
2023-09-23 03:47:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
+with open("test.txt", 'w') as f:
+    f.write("test")
 def test():
     with open("test.txt", 'r') as f:
         c = f.readlines()
